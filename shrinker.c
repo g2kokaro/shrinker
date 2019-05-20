f019dfbbe8678d6fdd34294b525cf312445fb45c
Dima Kokarovtsev <dima.kokarovtsev@gmail.com>
2019-05-20 01:19:06 -0400

    Implemented delta encoded shrinking and unshrinking functions. Doesn't actually shrink files yet.

--- shrinker.c
+++ shrinker.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+void shrink (FILE * in, FILE * out){
+  int previous = 0;
+  int current = fgetc(in);
+  while(!feof(in)){
+    fputc(current - previous, out);
+    previous = current;
+    current = fgetc(in);
+  }
+}
+
+void unshrink (FILE * in, FILE * out){
+  int previous = 0;
+  int delta = fgetc(in);
+  while(!feof(in)){
+    fputc(previous + delta, out);
+    previous = previous + delta;
+    delta = fgetc(in);
+  }
+}
 
 int main(int argc, char *argv[]) {
   if(argc < 4){
@@ -14,6 +35,12 @@ int main(int argc, char *argv[]) {
   FILE * in = fopen(inputFileName, "r");
   FILE * out = fopen(outputFileName, "a");
 
-  
+  if(strcmp(mode, "shrink") == 0){
+    shrink (in, out);
+  } else if (strcmp(mode, "unshrink") == 0) {
+    unshrink (in, out);
+  } else {
+    printf("Unknown mode: %s", mode);
+  }
   return 0;
 }
