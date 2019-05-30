d9b7c2246db4ead28a16535bd0b150062548b2f1
Dima Kokarovtsev <dima.kokarovtsev@gmail.com>
2019-05-29 21:20:19 -0400

    Added run length encoding to shrink function.

--- shrinker.c
+++ shrinker.c
@@ -1,17 +1,46 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define bufferSize 4096
 
-void shrink (FILE * in, FILE * out){
+void shrink (FILE *in, FILE *out){
+  char *inputBuffer = malloc(bufferSize);
+  unsigned char bufferIndex = 0;
+
   int previous = 0;
   int current = fgetc(in);
   while(!feof(in)){
-    fputc(current - previous, out);
+    inputBuffer[bufferIndex] = current - previous;
     previous = current;
     current = fgetc(in);
+    bufferIndex++;
+  }
+
+  previous = inputBuffer[0];
+  current = inputBuffer[1];
+  unsigned char count = 1;
+  for(int i = 1; i < bufferIndex; i++){
+    current = inputBuffer[i];
+    if(current == previous && count < 255){
+      count++;
+    } else {
+      if(count == 1){
+        if(previous == 0x00){
+          fputc(0x00, out);
+        }
+        fputc(previous, out);
+      } else {
+        fputc(0x00, out);
+        fputc(count, out);
+        fputc(previous, out);
+      }
+      count = 1;
+      previous = inputBuffer[i];
+    }
   }
 }
 
-void unshrink (FILE * in, FILE * out){
+void unshrink (FILE *in, FILE *out){
   int previous = 0;
   int delta = fgetc(in);
   while(!feof(in)){
@@ -32,12 +61,12 @@ int main(int argc, char *argv[]) {
   char *inputFileName = argv[2];
   char *outputFileName = argv[3];
 
-  FILE * in = fopen(inputFileName, "r");
-  FILE * out = fopen(outputFileName, "a");
+  FILE *in = fopen(inputFileName, "r");
+  FILE *out = fopen(outputFileName, "a");
 
   if(strcmp(mode, "shrink") == 0){
     shrink (in, out);
-  } else if (strcmp(mode, "unshrink") == 0) {
+  } else if(strcmp(mode, "unshrink") == 0) {
     unshrink (in, out);
   } else {
     printf("Unknown mode: %s", mode);
